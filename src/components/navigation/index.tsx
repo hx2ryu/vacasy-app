a0ff97fed15789a50b117d5ff463ce606487d794
hx2ryu <hx2ryu@gmail.com>
2021-11-14 21:58:08 +0900

    faet: add root navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootParamList} from './types';
 import {HomePage} from '@components/pages';
+import {COLORS} from '@/constants/theme';
 
+const Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: COLORS.primary,
+  },
+};
 const Stack = createNativeStackNavigator<RootParamList>();
 
 const RootNavigator: React.FC = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
+  <NavigationContainer theme={Theme}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="Home" component={HomePage} />
     </Stack.Navigator>
   </NavigationContainer>
 );
-
 export default RootNavigator;
