e6827876a19dfe2182306f69cc4d2a3d16cb69f9
hx2ryu <hx2ryu@gmail.com>
2021-11-30 22:34:24 +0900

    feat: set status-bar to dark-mode

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RootNavigator from '@/navigation';
 import store from '@/store';
 import {Provider} from 'react-redux';
+import {StatusBar} from 'react-native';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" />
         <RootNavigator />
       </SafeAreaProvider>
     </Provider>
