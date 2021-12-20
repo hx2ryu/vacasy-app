794333e1ae4c4904a373667fddd488b837205c7b
hx2ryu <hx2ryu@gmail.com>
2021-12-20 18:12:59 +0900

    chore: add hook dir

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = {
           '@store': './src/store',
           '@storage': './src/storage',
           '@features': './src/features',
+          '@hooks': './src/hooks',
           '@utils': './src/utils',
         },
       },
